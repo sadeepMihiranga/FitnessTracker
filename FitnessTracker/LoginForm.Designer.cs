e15d9a78479619d3fa1b347ceead02a5b78dfacd
Sadeep Mihiranga <saddeepmihiranga@gmail.com>
2023-06-23 22:24:51 +0530

    Add project files.

---
+++ FitnessTracker/LoginForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FitnessTracker
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.lblRegister = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FitnessTracker.Properties.Resources.logo_2;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FitnessTracker.Properties.Resources.icons8_user_642;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 194);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.panel1.Location = new System.Drawing.Point(21, 226);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::FitnessTracker.Properties.Resources.icons8_login_502;
+            this.pictureBox3.Location = new System.Drawing.Point(21, 268);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.panel2.Location = new System.Drawing.Point(21, 300);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 1);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(21, 354);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(250, 33);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUsername.Location = new System.Drawing.Point(58, 197);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(210, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPassword.Location = new System.Drawing.Point(57, 272);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(210, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // pictureBoxClose
+            // 
+            this.pictureBoxClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxClose.Image = global::FitnessTracker.Properties.Resources.icons8_close_128;
+            this.pictureBoxClose.Location = new System.Drawing.Point(261, 6);
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.Size = new System.Drawing.Size(27, 23);
+            this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxClose.TabIndex = 7;
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.AutoSize = true;
+            this.lblRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblRegister.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRegister.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblRegister.Location = new System.Drawing.Point(182, 404);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(89, 17);
+            this.lblRegister.TabIndex = 8;
+            this.lblRegister.Text = "Register Now";
+            this.lblRegister.Click += new System.EventHandler(this.lblRegister_Click);
+            this.lblRegister.MouseEnter += new System.EventHandler(this.lblRegister_MouseEnter);
+            this.lblRegister.MouseLeave += new System.EventHandler(this.lblRegister_MouseLeave);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(294, 450);
+            this.panel3.TabIndex = 9;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.lblRegister);
+            this.Controls.Add(this.pictureBoxClose);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.PictureBox pictureBoxClose;
+        private System.Windows.Forms.Label lblRegister;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
---
+++ FitnessTracker/LoginForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitnessTracker
+{
+    public partial class LoginForm : Form
+    {
+        private Label selectedLabel;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar = '*';
+
+            /* alert if capslock is on */
+            if (Control.IsKeyLocked(Keys.CapsLock))
+                MessageBox.Show("The Caps Lock key is ON.");
+        }
+
+        /** this will close login scrreen windows */
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Environment.Exit(0);
+        }
+
+        private void lblRegister_MouseEnter(object sender, EventArgs e)
+        {
+            var label = (Label)sender;
+            label.ForeColor = Color.FromArgb(32, 32, 32); //darkercolor
+        }
+
+        private void lblRegister_MouseLeave(object sender, EventArgs e)
+        {
+            var label = (Label)sender;
+            if (label == this.selectedLabel || this.selectedLabel == null) return;
+            label.ForeColor = Color.FromArgb(160, 160, 160); //lightercolor
+        }
+
+        /* open the customer registration form */
+        private void lblRegister_Click(object sender, EventArgs e)
+        {
+            /*CustomerRegister customerRegister = new CustomerRegister();
+            customerRegister.Show();
+            this.Hide();*/
+        }
+
+        /** login button click event */
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            /*User user = null;
+            List<Role> roles = null;
+            List<string> roleNameList = null;
+
+            string Username = txtUsername.Text;
+            string Password = txtPassword.Text;
+
+            if (String.IsNullOrEmpty(Username) || String.IsNullOrEmpty(Password))
+            {
+                MessageBox.Show("Username and Password required", "Missing Data", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                user = new User();
+                user = user.FetchUserByUsernameAndPassword(Username, Password);
+
+                roles = user.GetRolesByUserId(user.Id);
+                roleNameList = new List<string>();
+
+                foreach (Role role in roles)
+                    roleNameList.Add(role.RoleName);
+
+                if (user != null && user.Id > 0)
+                {
+                    MainMenuForm adminMenuForm = new MainMenuForm(roleNameList, user.PartyId);
+                    adminMenuForm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password invalid", "Operation Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                FormsHandler.ShowOperationFailedErrorMessage(ex.Message);
+            }
+            finally
+            {
+                if (roles != null)
+                    roles = null;
+
+                if (user != null)
+                    user = null;
+            }*/
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBorder(panel3, e);
+        }
+
+        public static void DrawBorder(Panel panel, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel.ClientRectangle,
+            Color.DimGray, 2, ButtonBorderStyle.Solid, // left
+            Color.DimGray, 2, ButtonBorderStyle.Solid, // top
+            Color.DimGray, 2, ButtonBorderStyle.Solid, // right
+            Color.DimGray, 2, ButtonBorderStyle.Solid);// bottom
+        }
+    }
+}
